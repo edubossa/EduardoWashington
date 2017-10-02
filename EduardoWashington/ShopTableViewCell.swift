da487c144ea974848b73ad5cc80e0cc89de21b9e
Eduardo Wallace Da Silva <eduardo.silva@ntk.com.br>
2017-10-02 17:24:12 -0300

    Initial commit

---
+++ EduardoWashington/ShopTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ShopTableViewCell.swift
+//  EduardoWashington
+//
+//  Created by Eduardo Wallace on 30/09/17.
+//  Copyright © 2017 Eduardo Wallace. All rights reserved.
+//
+
+import UIKit
+
+class ShopTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
