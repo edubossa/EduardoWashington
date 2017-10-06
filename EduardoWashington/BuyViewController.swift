50c18b799637b69f8413b702d712155f41a6d8ac
Eduardo Wallace Da Silva <eduardo.silva@ntk.com.br>
2017-10-06 00:24:59 -0300

    Adicionando tela ajuste e total da compra

---
+++ EduardoWashington/BuyViewController.swift
@@ -0,0 +1,20 @@
+//
+//  BuyViewController.swift
+//  EduardoWashington
+//
+//  Created by Eduardo Wallace on 05/10/2017.
+//  Copyright © 2017 Eduardo Wallace. All rights reserved.
+//
+
+import UIKit
+
+class BuyViewController: UIViewController {
+    
+    @IBOutlet weak var tfTotalUS: UILabel!
+    @IBOutlet weak var tfTotalRS: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
---
+++ EduardoWashington/SettingsViewController.swift
@@ -0,0 +1,26 @@
+//
+//  SettingsViewController.swift
+//  EduardoWashington
+//
+//  Created by Eduardo Wallace on 05/10/2017.
+//  Copyright © 2017 Eduardo Wallace. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    
+    @IBOutlet weak var tfDolarQuotation: UITextField!
+    @IBOutlet weak var tfIOF: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    
+    @IBAction func addState(_ sender: UIButton) {
+        
+    }
+    
+}
